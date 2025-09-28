f7aefc7d88caca57c87a6132d4ac29fbb17ae039
张志强 <zhangzhiqiang.mail@qq.com>
2025-09-28 17:03:01 +0800

    init

---
+++ Example/Pods/Target Support Files/KiwiPublicPod/KiwiPublicPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_KiwiPublicPod : NSObject
+@end
+@implementation PodsDummy_KiwiPublicPod
+@end
---
+++ KiwiPublicPod/Classes/Common.swift
@@ -0,0 +1,15 @@
+//
+//  Common.swift
+//  MobileProgect
+//
+//  Created by csqiuzhi on 2019/4/30.
+//  Copyright © 2019 于晓杰. All rights reserved.
+//
+
+import UIKit
+// MARK: - APP 配置相关
+let kkMainColor = "#06D094"
+let kkMainTextColor = "#202124"
+let kkTextSubColor = "5B5F65"
+let kkLightColor = "E0E2E5"
+
